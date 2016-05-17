2cc17bb808289f6b7f922d2f808ed82f11d80466
Andrew Cairns <andrew@acairns.co.uk>
2016-05-17 09:53:08 +0100

    Add a quick test to get integration tools setup

---
+++ tests/SetupTest.php
@@ -0,0 +1,9 @@
+<?php namespace Cairns\Radiate\Tests;
+
+class SetupTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_nothing_really()
+    {
+        $this->assertTrue(true);
+    }
+}
