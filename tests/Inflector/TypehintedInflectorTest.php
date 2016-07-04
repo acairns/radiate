46ef160e492aa9a79da39ca045a054f15aa4525a
Andrew Cairns <andrew@acairns.co.uk>
2016-07-04 22:02:50 +0100

    Assert inflector ignores blank classes and methods

--- tests/Inflector/TypehintedInflectorTest.php
+++ tests/Inflector/TypehintedInflectorTest.php
@@ -17,4 +17,38 @@ class TypehintedInflectorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertContains('handle', $method);
     }
+
+    public function test_it_ignores_listeners_without_any_methods()
+    {
+        $inflector = new TypehintedInflector;
+
+        $method = $inflector->inflect(
+            new RegularEvent,
+            new ListenerWithoutAnyMethods
+        );
+
+        $this->assertNull($method);
+    }
+
+    public function test_it_ignores_listener_methods_without_parameters()
+    {
+        $inflector = new TypehintedInflector;
+
+        $method = $inflector->inflect(
+            new RegularEvent,
+            new ListenerWithoutAnyMethods
+        );
+
+        $this->assertNull($method);
+    }
+}
+
+class ListenerWithoutAnyMethods {}
+
+class ListenerWithMethodWithoutParameters
+{
+    public function handleSomething()
+    {
+
+    }
 }
