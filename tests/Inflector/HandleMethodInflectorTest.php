756481a98895f1fbeb344f8b141cc6996caa81ed
Andrew Cairns <andrew@acairns.co.uk>
2016-07-22 17:48:21 +0100

    Ensure nothing is returned when handle method does not exist on listener

--- tests/Inflector/HandleMethodInflectorTest.php
+++ tests/Inflector/HandleMethodInflectorTest.php
@@ -17,4 +17,21 @@ class HandleMethodInflectorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('handle', $methodName);
     }
+
+    public function test_it_does_not_return_the_handle_method_if_it_does_not_exist_on_the_listener()
+    {
+        $inflector = new HandleMethodInflector;
+
+        $methodName = $inflector->inflect(
+            new RegularEvent,
+            new ListenerWithoutHandleMethod
+        );
+
+        $this->assertNull($methodName);
+    }
+}
+
+class ListenerWithoutHandleMethod
+{
+
 }
